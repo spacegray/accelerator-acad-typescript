29ac8125891b3ef3681dcbdbdeea05c75530c55e
Ayla Basha <aylabasha@gmail.com>
2023-04-24 13:50:38 -0700

    complete

--- animals.ts
+++ animals.ts
@@ -1,43 +1,45 @@
 import { dog, cat, bird, pigeon, anyAnimal } from "./animals_data";
 
 // Part 1: Remove the errors in the function below by filling the conditions of the if statements on lines 6, 13, and 20
-function classifyAnimal(animal: anyAnimal) {
+function classifyAnimal(animal: { [key: string]: any }) {
 
-    if () {
-
-        animal.barks
-        console.log('This is a dog')
-        return
-    }
-
-    if () {
-
-        animal.coos
-        console.log('This is a pigeon')
-        return
-    }
-
-    if () {
-
-        animal.meows
-        console.log('This is a cat')
-        return
+    if ("barks" in animal) {
+        animal.barks = true;
+        console.log("This is a dog");
+    } else if ("coos" in animal) {
+        animal.coos = true;
+        console.log("This is a pigeon");
+    } else if ("meows" in animal) {
+        animal.meows = true;
+        console.log("This is a cat");
     }
 
 }
 
 
 // Part 2: Uncomment the lines below and test the classifyAnimal function by creating dog, pigeon, and cat objects
-// const fido = {
+const fido = {
+    name: "Fido",
+    color: "tan",
+    barks: true,
+    hasTail: true
+}
+classifyAnimal(fido) // should print 'This is a dog'
 
-// }
-// classifyAnimal(fido) // should print 'This is a dog'
+const whiskers = {
+    name: "Whiskers",
+    color: "black",
+    meows: true,
+    hasTail: true
+}
+classifyAnimal(whiskers) // should print 'This is a cat'
 
-// const whiskers = {
-// }
-// classifyAnimal(whiskers) // should print 'This is a cat'
+const skyrat = {
+    name: "Skyrat",
+    color: "grey",
+    canFly: true,
+    coos: true
 
-// const skyrat = {
+}
+classifyAnimal(skyrat) // should print 'This is a pigeon'
 
-// }
-// classifyAnimal(skyrat) // should print 'This is a pigeon'
